65ebf8576e13edd82d42901f820e0220c8978cbe
Arthur Santos <amsantos@thoughtworks.com>
2015-04-10 23:23:17 -0300

    [Passing] Attendee should belong to a Workshop

--- app/models/attendee.rb
+++ app/models/attendee.rb
@@ -1,3 +1,5 @@
 class Attendee < ActiveRecord::Base
+  belongs_to :workshop
+
   validates :name, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150411001844) do
+ActiveRecord::Schema.define(version: 20150411022157) do
 
   create_table "attendees", force: true do |t|
     t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "workshop_id"
   end
+
+  add_index "attendees", ["workshop_id"], name: "index_attendees_on_workshop_id"
 
   create_table "workshops", force: true do |t|
     t.string   "name"
