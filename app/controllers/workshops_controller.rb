2b0ba4ef4c3ee256912ad4299cbd95118723b763
Arthur Santos <amsantos@thoughtworks.com>
2015-04-11 17:04:16 -0300

    Adding actions for workshop creation

--- app/controllers/workshops_controller.rb
+++ app/controllers/workshops_controller.rb
@@ -4,14 +4,26 @@ class WorkshopsController < ApplicationController
   end
 
   def new
+    @workshop = Workshop.new
   end
 
   def create
+    @workshop = Workshop.create(workshop_params)
+    if @workshop.persisted?
+      redirect_to workshops_path
+    else
+      render 'new'
+    end
   end
 
   def update
   end
 
   def delete
+  end
+
+  private
+  def workshop_params
+    params.require(:workshop).permit(:name)
   end
 end
